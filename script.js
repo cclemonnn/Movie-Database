91091a6bc1ba6f307d628a2d81cbe505cf3b4425
cclemonnn <petersocool0313@gmail.com>
2022-12-31 04:42:39 +0800

    added urls

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-console.log(123);
+import API_KEY from "./secrets.js";
+
+const API_URL = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&page=1`;
+const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&include_adult=false&query=`;
