7c3f46376a4532aa1c66e062d196678a20365ccd
cclemonnn <petersocool0313@gmail.com>
2023-01-01 03:11:46 +0800

    init get movies and search

--- script.js
+++ script.js
@@ -1,5 +1,42 @@
 import API_KEY from "./secrets.js";
 
+// API URLs
 const API_URL = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&page=1`;
 const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&include_adult=false&query=`;
+
+// elements
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+
+getMovies(API_URL);
+
+async function getMovies(url) {
+  try {
+    const res = await fetch(url);
+    if (!res.ok) {
+      console.log("status: " + res.status);
+      throw Error;
+    }
+
+    const data = await res.json();
+    console.log(data.results);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// search function
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const searchTerm = search.value;
+
+  if (searchTerm && searchTerm !== "") {
+    getMovies(SEARCH_API + searchTerm);
+
+    search.value = "";
+  } else {
+    window.location.reload();
+  }
+});
