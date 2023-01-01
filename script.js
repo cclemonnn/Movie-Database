ee89c021d5f3fb6c9db1429e97eeca79aefec8d0
cclemonnn <petersocool0313@gmail.com>
2023-01-02 04:40:10 +0800

    next btn

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 import API_KEY from "./secrets.js";
 
 // API URLs
-const API_URL = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&page=1`;
+// const API_URL = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&page=1`;
+const API_URL = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&&include_adult=false`;
 const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&include_adult=false&query=`;
 
@@ -9,21 +10,52 @@ const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}
 const form = document.getElementById("form");
 const search = document.getElementById("search");
 const main = document.getElementById("main");
-const searchText = document.querySelector(".search-text");
+const searchText = document.getElementsByClassName("search-text");
+const prev1 = document.getElementById("prev1");
+const prev2 = document.getElementById("prev2");
+const next1 = document.getElementById("next1");
+const next2 = document.getElementById("next2");
+const currentPageText = document.querySelectorAll(".current-page");
 
-getMovies(API_URL);
+// current URL
+let currentURL = API_URL;
+let currentPage = 1;
+let totalPage = 15;
 
-async function getMovies(url) {
+// get movies
+getMovies(API_URL, 1);
+
+async function getMovies(url, page, sort) {
+  let fullURL = url;
   try {
-    const res = await fetch(url);
+    if (page) {
+      fullURL = fullURL + "&page=" + page;
+    }
+
+    if (sort) {
+      fullURL = fullURL + "&sort_by=" + sort;
+    }
+
+    const res = await fetch(fullURL);
     if (!res.ok) {
       console.log("status: " + res.status);
       throw Error;
     }
 
     const data = await res.json();
-    showMovies(data.results);
-    console.log(data.results);
+    if (data.total_pages > 0) {
+      console.log(data.results);
+      console.log(data);
+      if (data.total_pages > 15) {
+        totalPage = 15;
+      } else {
+        totalPage = data.total_pages;
+      }
+      showMovies(data.results);
+    } else {
+      // need style
+      main.innerText = "no results";
+    }
   } catch (error) {
     console.error(error);
   }
@@ -60,6 +92,7 @@ function showMovies(movies) {
   });
 }
 
+// rating color
 function getClassByRate(rate) {
   if (rate < 5) {
     return "red";
@@ -77,11 +110,35 @@ form.addEventListener("submit", (e) => {
   const searchTerm = search.value;
 
   if (searchTerm && searchTerm !== "") {
-    getMovies(SEARCH_API + searchTerm);
+    currentURL = SEARCH_API + searchTerm;
+    currentPage = 1;
+    getMovies(currentURL);
 
     searchText.innerText = `Search Results: ${searchTerm}`;
     search.value = "";
   } else {
-    window.location.reload();
+    getMovies(currentURL, currentPage);
   }
 });
+
+// next page btn
+next1.addEventListener("click", showNextPage);
+next2.addEventListener("click", showNextPage);
+
+function showNextPage() {
+  if (currentPage < totalPage) {
+    console.log(currentPage);
+    currentPage++;
+    incrementPageText();
+  }
+  if (currentPage <= totalPage) {
+    getMovies(currentURL, currentPage);
+    window.scrollTo({ top: 0, behavior: "instant" });
+  }
+}
+
+function incrementPageText() {
+  currentPageText.forEach((page) => {
+    page.innerText = currentPage;
+  });
+}
